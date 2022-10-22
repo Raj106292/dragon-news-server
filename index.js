a5e94c8f709f5c17c6ac44b093bfd0c2feac758f
Raj106292 <rajdas.106292@gmail.com>
2022-10-23 00:55:06 +0600

    sever done

--- index.js
+++ index.js
@@ -7,13 +7,36 @@ app.use(cors());
 
 const categories = require('./data/categories.json');
 
+const news = require('./data/news.json');
+
 app.get('/', (req, res) => {
     res.send('News API running');
 });
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
-})
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+});
 
 app.listen(port, () => {
     console.log(`news api running on port ${port}`);
